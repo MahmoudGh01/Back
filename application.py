882320a1d7f18239c59839a6e7e8c6f8b3e4d341
MahmoudGh01 <mahmoud.gharbi@esprit.tn>
2024-03-26 01:26:26 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
 
 
+import os
 from app import app
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT not found
+    app.run(host='0.0.0.0', port=port)
